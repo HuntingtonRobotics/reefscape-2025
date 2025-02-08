e48844185d351983767703fc2ffd05a4307a49f1
Huntington Robotics MP <huntingtonrobotics@gmail.com>
2025-02-08 13:39:26 -0500

    Scaffold updates for door vs ramp

--- src/main/java/frc/robot/subsystems/CoralDoor.java
+++ src/main/java/frc/robot/subsystems/CoralDoor.java
@@ -6,18 +6,18 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class CoralRamp extends SubsystemBase {
-    private Solenoid rampSolenoid; // Or DoubleSolenoid
+public class CoralDoor extends SubsystemBase {
+    private Solenoid doorSolenoid; // Or DoubleSolenoid
 
-    public CoralRamp() {
-        rampSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+    public CoralDoor() {
+        doorSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
     }
 
-    public Command raise() {
+    public Command up() {
         return Commands.none();
     }
 
-    public Command lower() {
+    public Command down() {
         return Commands.none();
     }
 }
